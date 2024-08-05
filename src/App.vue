03eeccf4d2fc1d729bfe4a88a79c75696d969b6f
NguyenChiThuan0509 <ncthuan0509@gmail.com>
2024-08-05 09:16:11 +0700

    Commit message

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import Header from "./components/Header.vue"
+import HelloWorld from './components/HelloWorld.vue'
+import Left from './components/Left.vue'
+</script>
+
+<template>
+  <!-- #0 -->
+  <Header />
+  <!-- #1 -->
+  <div>
+    <a href="/">
+      <img src="./assets/Jon..png" class="logo" alt="web logo">
+    </a>
+  </div>
+  <!-- #2 -->
+  <HelloWorld msg="Call me Jon." />
+  <!-- #3 -->
+  <Left msg="Let's explore" />
+</template>
+
+<style scoped>
+.logo {
+  height: 10em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+
+  border-radius: 50%;
+  /* bo logo */
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<script></script>
+
+<template>
+
+    <div class="header">
+        <a href="#home">Home</a>
+        <a href="#project">Project</a>
+        <a href="#contact">Contact</a>
+    </div>
+
+</template>
+
+<style>
+.header {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 20px;
+    background-color: rgba(253, 48, 4, 0.8);
+    border-radius: 20px;
+    margin: 20px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+.header a {
+    text-decoration: none;
+    color: #333;
+    font-weight: bolder;
+    font-size: larger;
+    padding: 10px 20px;
+}
+
+.header a:hover {
+    background-color: #ddd;
+    border-radius: 10px;
+}
+</style>
